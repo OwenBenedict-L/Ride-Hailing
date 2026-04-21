36fbc241ff32643ce577c559dec3699e4ee82067
Elfrandt <elfrandtgold29@gmail.com>
2026-04-21 22:00:42 +0700

    Merging Endpoints

--- src/api/components/auth/auth-route.js
+++ src/api/components/auth/auth-route.js
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
 const express = require('express');
-=======
-const express = require('express')
->>>>>>> d19c15ede408254d66d580f64098b18e586646e9
 const authController = require('./auth-controller');
 const { authMiddleware } = require('../../middlewares');
 
 const route = express.Router();
 
 module.exports = (app) => {
-<<<<<<< HEAD
   app.use('/auth', route);
 
   route.post('/register', authController.register);
   route.post('/login', authController.login);
   route.get('/protected', authMiddleware, authController.testProtected);
 };
-=======
-    app.use('/auth', route);
-
-    route.post('/loginDriver', authController.loginDriver);
-    route.get('/protectedDriver', authMiddleware, authController.testProtectedDriver);
-};
->>>>>>> d19c15ede408254d66d580f64098b18e586646e9
