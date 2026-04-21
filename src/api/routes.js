5154d37701bc6378671e914b94a9e2f20091c73f
SheireenSadeli <sheireensadeli@gmail.com>
2026-04-21 16:15:40 +0700

    Bookings & Notifications

--- src/api/routes.js
+++ src/api/routes.js
@@ -2,21 +2,21 @@ const express = require('express');
 
 const books = require('./components/books/books-route');
 const users = require('./components/users/users-route');
-const estimations = require('./components/estimations/estimations-route');
 const wallet = require('./components/wallet/wallet-route');
 const review = require('./components/review/review-route');
 const bookings = require('./components/bookings/bookings-route');
 const notifications = require('./components/notifications/notifications-route');
+const estimations = require('./components/estimations/estimations-route');
 
 module.exports = () => {
   const app = express.Router();
-  estimations(app);
   books(app);
   users(app);
   wallet(app);
   review(app);
   bookings(app);
   notifications(app);
+  estimations(app);
 
   return app;
 };
